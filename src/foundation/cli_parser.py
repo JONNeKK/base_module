001391d2671b0377eb2d4116c8468b28b35d95c3
Jannek Schaffert <121396590+JONNeKK@users.noreply.github.com>
2026-02-17 16:13:06 +0000

    parse to correct fields
    Calling BaseConfig.convert2fields on the data extracted from argparse. This allows to collect the data in other formats and converting it before initializing the Config class

--- src/foundation/cli_parser.py
+++ src/foundation/cli_parser.py
@@ -70,6 +70,7 @@ class BaseCLIParser(Generic[TConfig]):
         args.extras = extras
 
         cli_args = self.parse_base(args)
+        self.config_class.convert2fields(cli_args)
         cfg = self.config_class(**cli_args)
         cfg.cmd_args = cli_args
         return cfg
