7b313051a0fb3857df67c3d8137d288fd1712a1d
Jannek Schaffert <121396590+JONNeKK@users.noreply.github.com>
2026-03-06 10:35:09 +0000

    API exposition of utility functions
    Both configuration and git.

--- src/foundation/utils/__init__.py
+++ src/foundation/utils/__init__.py
@@ -1,2 +1,4 @@
 from .filesystem import ensure_dir_exists, maybe_ensure_dir_exists, safe_ensure_dir_exists, remove_if_exists, ensure_parents_exist
 from .parsing import str2bool, is_dataclass_type
+from .configuration import deep_merge, apply_overwrite
+from .git import get_git_commit_hash
