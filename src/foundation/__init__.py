27f3fc6f904ede3a752ccc1e066b97f1ca269e73
Jannek Schaffert <121396590+JONNeKK@users.noreply.github.com>
2026-09-15 17:21:58 +0200

    Exposition Fix
    Remove DEFAULT_CONVERTERS from __init__.py

--- src/foundation/__init__.py
+++ src/foundation/__init__.py
@@ -1,4 +1,4 @@
 from .config import BaseConfig
-from .utils.configuration import CustomJSONEncoder, DEFAULT_CONVERTERS, DEFAULT_CAST
+from .utils.configuration import CustomJSONEncoder, DEFAULT_CAST
 
 from .cli_parser import BaseCLIParser, ParamFunc
